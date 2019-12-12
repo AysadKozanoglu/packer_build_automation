69b421bd7ce483fd9a2da6a3af790cffae05187f
Aysad Kozanoglu <aysadx@gmail.com>
2019-12-13 00:52:31 +0100

    TEMPLATE_FILENAME

--- update-iso-checksum.sh
+++ update-iso-checksum.sh
@@ -18,11 +18,11 @@
 TEMPLATE_FILENAME="packer-debStretch-qemu-kvm.json"
 
 
-for paket in jq curl sed awk; do which jq || { echo "HATA: '$paket' bulunamadi"; exit 1; }; done
+for paket in jq curl sed awk; do which $paket || { echo "HATA: '$paket' bulunamadi"; exit 1; }; done
 
-iso_checksum_type="$(jq --raw-output '.variables.iso_checksum_type' ${template_filename})"
-     iso_file_url="$(jq --raw-output '.variables.dynamic_mirror_url' ${template_filename})"
-    iso_file_name="$(jq --raw-output '.variables.iso_name' ${template_filename})"
+iso_checksum_type="$(jq --raw-output '.variables.iso_checksum_type' ${TEMPLATE_FILENAME})"
+     iso_file_url="$(jq --raw-output '.variables.dynamic_mirror_url' ${TEMPLATE_FILENAME})"
+    iso_file_name="$(jq --raw-output '.variables.iso_name' ${TEMPLATE_FILENAME})"
  new_iso_checksum=$(curl -svL ${iso_file_url}/$(echo ${iso_checksum_type} | awk '{print toupper($0)}')SUMS | grep ${iso_file_name} | awk '{ print $1 }')
 
 sed -r -e "s/(\"iso_checksum\"\:\ \")([a-fA-F0-9]+)(\",)/\1${new_iso_checksum}\3/g" -i ${TEMPLATE_FILENAME}
